75aff6ffc2b42bcdc8b64f4b35073d54be484399
Mikhail Bondarenko <m.a.bondarenko@outlook.com>
2022-04-30 20:11:16 +0300

    Выполнены задания 1 и 2

--- sb-homework02/SB_Homework02/Program.cs
+++ sb-homework02/SB_Homework02/Program.cs
@@ -6,7 +6,34 @@ namespace SB_Homework02
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Заводим переменные для хранения информации
+            // личная информация
+            string fullName = "Иваненко Иван Иванович";
+            int age = 27;
+            string email = "ivanenko@yandex.ru";
+            // баллы за предметы
+            double programPoints = 36;
+            double mathPoints = 47;
+            double physPoints = 66;
+
+            // Объявляем и инициализируем переменную для суммы баллов
+            double sumPoints = programPoints + mathPoints + physPoints;
+            // Считаем средний балл за три предмета
+            double averagePoints = sumPoints / 3;
+
+            // вывод данных на экран
+            Console.WriteLine("Информация о студенте {0}", fullName);
+            Console.WriteLine("Возраст: {0}", age);
+            Console.WriteLine("Электронный адрес: {0}", email);
+            Console.WriteLine("Баллы за пройденные курсы");
+            Console.WriteLine("Программирование: {0}", programPoints);
+            Console.WriteLine("Математика: {0}", mathPoints);
+            Console.WriteLine("Физика {0}", physPoints);
+            Console.WriteLine("-----------");
+            Console.WriteLine("Баллов всего: {0}", sumPoints);
+            Console.WriteLine("Средний балл: {0:00}", averagePoints);
+
+            Console.ReadKey();
         }
     }
 }
