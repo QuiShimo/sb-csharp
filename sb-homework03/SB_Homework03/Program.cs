220b76f5ceb8947b326f73b8e21d452987f2c9d1
Mikhail Bondarenko <m.a.bondarenko@outlook.com>
2022-05-01 22:30:42 +0300

    Добавьте файлы проекта.

---
+++ sb-homework03/SB_Homework03/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace SB_Homework03.Exercise_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Задание 1
+            Console.WriteLine("Приложение по определению чётного или нечётного числа");
+
+            while (true)
+            {
+                Console.Write("Введите число: ");
+                // проверяем удалось ли преобразовать строку в число
+                bool check = int.TryParse(Console.ReadLine(), out int number);
+
+                if (check)
+                {
+                    // если удалось получить число, проверяем на четность
+
+                    if (number % 2 == 0)
+                    {
+                        Console.WriteLine("Число четное!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Число нечетное!");
+                    }
+                    
+                    // выходим из цикла
+                    break;
+                }
+                else
+                {
+                    // если приобразование не удалось, просим повторить ввод числа
+                    Console.Write("Повторите ввод числа: ");
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
