78333e02e733a1f64cde1b35b7c9fd10da09126b
Mikhail Bondarenko <m.a.bondarenko@outlook.com>
2022-05-03 19:48:40 +0300

    Добавлено решение первой задачи

--- sb-homework04/Exercise_01/Program.cs
+++ sb-homework04/Exercise_01/Program.cs
@@ -6,7 +6,41 @@ namespace Exercise_01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n, m, sum = 0;
+            int[,] matrix;
+            Random random = new Random();
+
+            Console.Write("Введите количество строк: ");
+            n = InputNumber();
+
+            Console.Write("Введите количество столбцов: ");
+            m = InputNumber();
+
+            matrix = new int[n, m];
+
+            Console.WriteLine("Вывод матрицы размером {0}x{1}", n, m);
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    matrix[i, j] = random.Next(0, 10);
+                    sum += matrix[i, j];
+                    Console.Write("{0} ", matrix[i, j]);
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Сумма элементов: {0}", sum);
+        }
+
+        private static int InputNumber()
+        {
+            int number;
+
+            while (!int.TryParse(Console.ReadLine(), out number))
+                Console.Write("Повторите ввод: ");
+
+            return number;
         }
     }
 }
