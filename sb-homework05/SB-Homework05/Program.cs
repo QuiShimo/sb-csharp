26d8a7a006abf579a7a6fec015e2098081d5fa98
Mikhail Bondarenko <m.a.bondarenko@outlook.com>
2022-05-04 21:54:13 +0300

    Добавьте файлы проекта.

---
+++ sb-homework05/SB-Homework05/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SB_Homework05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
