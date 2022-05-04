741452d78ceea20ae417c1604b86c53f697957f7
Mikhail Bondarenko <m.a.bondarenko@outlook.com>
2022-05-04 23:28:53 +0300

    Добавлено решение задания 1 и 2

--- sb-homework05/SB-Homework05/Program.cs
+++ sb-homework05/SB-Homework05/Program.cs
@@ -6,7 +6,35 @@ namespace SB_Homework05
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Введите предложение: ");
+            string inputPhrase = Console.ReadLine();
+
+            Console.WriteLine("Предложение по словам:");
+            PrintWords(SplitString(inputPhrase));
+
+            Console.WriteLine("Предложение по словам, в обратном порядке:");
+            PrintWords(ReversWords(inputPhrase));
+        }
+
+        public static string[] SplitString(string inputPhrase)
+        {
+            return inputPhrase.Split(' ');
+        }
+
+        public static void PrintWords(string[] words)
+        {
+            foreach (string word in words)
+                Console.WriteLine(word);
+        }
+
+        public static string[] ReversWords(string inputPhrase)
+        {
+            string[] reverseString;
+
+            reverseString = SplitString(inputPhrase);
+            Array.Reverse(reverseString);
+
+            return reverseString;
         }
     }
 }
